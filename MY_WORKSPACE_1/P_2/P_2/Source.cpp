77b113c3b4e9e16b760f1c5f0611dd3393feda04
Muhammad Sayed Ahmed <38951714+MuhammadSayedA@users.noreply.github.com>
2020-08-23 16:29:28 +0200

    Add files via upload

---
+++ MY_WORKSPACE_1/P_2/P_2/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//using namespace std;
+int main()
+{
+	int x; 
+	std::cout << "Enter Your Fav. No. between 1 & 100: " << std::endl;
+	std::cin >> x;
+	std::cout << "AMAZING! " << std::endl;
+	std::cout << "REALLY!" << x << std::endl;
+	return 0;
+}
---
+++ MY_WORKSPACE_1/P_3/P_3/Source.cpp
@@ -0,0 +1,18 @@
+/* 
+the
+line
+comment
+*/
+//comment
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y,z;
+	cout << "enter no.s" <<endl;
+	cin >> x >> y >> z;
+	cout << "x = "<< x << ",y = " << y << ",z = " << z <<endl;
+
+	return 0;
+}
